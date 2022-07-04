3f6adca4c0c6c0b15ef97172650596af9fcc7b8e
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-04 15:43:08 +0430

    linters errors fixed

--- js/index.js
+++ js/index.js
@@ -5,26 +5,26 @@ import { DateTime } from './modules/luxon.js';
 import * as model from './modules/model.js';
 
 const addBook = () => {
-    const [title, author] = bookAddView.getFormValues();
-    if (!title || !author) return;
-    model.addBook(title, author);
-    bookView.render(model.state.books);
-  };
+  const [title, author] = bookAddView.getFormValues();
+  if (!title || !author) return;
+  model.addBook(title, author);
+  bookView.render(model.state.books);
+};
 
-  const removeBook = (id) => {
-    model.removeBook(id);
-    bookView.render(model.state.books);
-  };
-  
-  const init = () => {
-    model.loadBooks();
-    bookView.render(model.state.books);
-    navigationView.navigate();
-  
-    navigationView.setYear(DateTime.now());
-  
-    bookAddView.addHandlerAdder(addBook);
-    bookView.addHandlerRemover(removeBook);
-  };
-  
-  init();
+const removeBook = (id) => {
+  model.removeBook(id);
+  bookView.render(model.state.books);
+};
+
+const init = () => {
+  model.loadBooks();
+  bookView.render(model.state.books);
+  navigationView.navigate();
+
+  navigationView.setYear(DateTime.now());
+
+  bookAddView.addHandlerAdder(addBook);
+  bookView.addHandlerRemover(removeBook);
+};
+
+init();
