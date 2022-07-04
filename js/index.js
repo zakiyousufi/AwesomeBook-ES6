36c103efff148ac195b48b08b1f85345cc2279a7
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-04 15:30:57 +0430

    model file imported on js file

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import bookAddView from './modules/view/bookAddView';
 import bookView from './modules/view/bookView';
 import navigationView from './modules/view/navigationView';
 import { DateTime } from './modules/luxon.js';
+import * as model from './modules/model.js';
 
 const addBook = () => {
     const [title, author] = bookAddView.getFormValues();
@@ -14,4 +15,17 @@ const addBook = () => {
     model.removeBook(id);
     bookView.render(model.state.books);
   };
+  
+  const init = () => {
+    model.loadBooks();
+    bookView.render(model.state.books);
+    navigationView.navigate();
+  
+    navigationView.setYear(DateTime.now());
+  
+    bookAddView.addHandlerAdder(addBook);
+    bookView.addHandlerRemover(removeBook);
+  };
+  
+  init();
   
