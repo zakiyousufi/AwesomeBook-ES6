5edbba846b695eae9071fcb3be6fb744f8725edf
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-04 15:24:59 +0430

    Add book fanction added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import bookAddView from './modules/view/bookAddView';
 import bookView from './modules/view/bookView';
 import navigationView from './modules/view/navigationView';
+
+const addBook = () => {
+    const [title, author] = bookAddView.getFormValues();
+    if (!title || !author) return;
+    model.addBook(title, author);
+    bookView.render(model.state.books);
+  };
