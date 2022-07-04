14e3ae577f709bee153f64776fafa92ae57c5dd9
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-04 15:28:25 +0430

    luxon file imported on js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import bookAddView from './modules/view/bookAddView';
 import bookView from './modules/view/bookView';
 import navigationView from './modules/view/navigationView';
+import { DateTime } from './modules/luxon.js';
 
 const addBook = () => {
     const [title, author] = bookAddView.getFormValues();
